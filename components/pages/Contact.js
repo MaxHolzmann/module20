2a561dae478e09979168c0a9924f83602deaf3e6
Maximilian Holzmann <max@holzmann.io>
2023-03-26 10:07:09 -0500

    Finishs contact page temporarily

--- components/pages/Contact.js
+++ components/pages/Contact.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import styles from '../../styles/styles.module.css'
 
 
 function Contact() {
   return (
    <>
-    
+    <h1 className={styles['contact-header']}>Contact Me</h1>
+    <p className={styles['contact-text']}>Use any of the contact methods below to contact me!</p>
+    <p className={styles['contact-methods']}><a href="mailto:max@holzmann.io">max@holzmann.io</a></p>
+    <p className={styles['contact-methods']}><a href="https://github.com/MaxHolzmann">GitHub</a></p>
    </>
   );
 }
