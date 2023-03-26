bf096ea0629828d7c66fcd59811b2354ee6cfc37
Maximilian Holzmann <max@holzmann.io>
2023-03-26 10:10:56 -0500

    adds resume to contact page

--- components/pages/Contact.js
+++ components/pages/Contact.js
@@ -8,7 +8,8 @@ function Contact() {
     <h1 className={styles['contact-header']}>Contact Me</h1>
     <p className={styles['contact-text']}>Use any of the contact methods below to contact me!</p>
     <p className={styles['contact-methods']}><a href="mailto:max@holzmann.io">max@holzmann.io</a></p>
-    <p className={styles['contact-methods']}><a href="https://github.com/MaxHolzmann">GitHub</a></p>
+    <p className={styles['contact-methods']}><a target="_blank" href="https://github.com/MaxHolzmann">GitHub</a></p>
+    <p className={styles['contact-methods']}><a target="_blank" href="https://resume.io/r/HEv3It4mA">Resume</a></p>
    </>
   );
 }
