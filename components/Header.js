d6144248a0ea8638a17694acb6b5f0f2ea809939
Maximilian Holzmann <max@holzmann.io>
2023-05-14 22:22:36 -0500

    adds biography to bio page

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ function Header({currentPage, handlePageChange}) {
     <nav className={styles['nav']}>
    <h1>Maximilian Holzmann</h1>
    <Button className={styles.navLink} onClick={() =>handlePageChange('Home')} variant="text">Home</Button>
-   <Button className={styles.navLink} onClick={() =>handlePageChange('About')} variant="text">About</Button>
+   <Button className={styles.navLink} onClick={() =>handlePageChange('About')} variant="text">Bio</Button>
    <Button className={styles.navLink} onClick={() => handlePageChange('Portfolio')} variant="text">Portfolio</Button>
    <Button  className={styles.navLink} onClick={() =>handlePageChange('Contact')} variant="text">Contact</Button>
    </nav>
--- components/pages/About.js
+++ components/pages/About.js
@@ -8,7 +8,9 @@ function About() {
    <h1 className={styles['about-header']}>Maximilian Holzmann</h1>
    <div className={styles['about-container']}>
     <img className={styles['about-img']} src='./profile-pic.png' alt="Icon" />
-    <p className={styles['about-bio']}>This is my test bio. I don't have a bio written, but I want it to be lengthy, so I will continue to type things. Hello, bio bio bio!</p>
+    <p className={styles['about-bio']}>Hello! My name is Maximilian Holzmann, and I'm based out of Fond du Lac, Wisconsin. I am currently a recent graduate of University of Minnesota's Full Stack Bootcamp for web development.
+    Outside of Web Development, I have many hobbies. I love to sing with my community choir and with my acapella quartet. I prioritize working out and staying active every day, through many activities such as running or pick up basketball. I also volunteer on the Board of Directors in two community organizations, through South Shore Chorale and United Way Fond du Lac.
+I am also an active Real Estate Investor, and hold my Real Estate license in the state of Wisconsin.</p>
    </div>
    </>
   );
